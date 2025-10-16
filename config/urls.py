7966b62d88e1ac94e7b60b9bf39caa4d1ab82321
hoyeonsoft <50389104+hys-dev@users.noreply.github.com>
2025-10-16 16:48:29 +0900

    로그인페이지 설정

--- config/urls.py
+++ config/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', include('sejong.urls')),
+    path('', include('sejong.urls')),
     path('users/', include('users.urls')),
 ]
